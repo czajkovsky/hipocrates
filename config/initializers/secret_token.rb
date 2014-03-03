5ce2460dcc31f6cbd56b75c42f5677160857ce62
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-03 10:48:41 +0100

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hipocrates::Application.config.secret_key_base = 'bd8156040f0c498fcb8728b4e3976a1f32f5f61d69dbb69ed140d0595b8095db6ea300948d317f6583f9a79730f3dfbc204535a110e6eee5538d160149f9670b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hipocrates::Application.config.session_store :cookie_store, key: '_hipocrates_session'
