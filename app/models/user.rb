aab3032b5cf119f4352be7577b46c26bd33882bc
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-09 17:22:41 +0100

    complete fields for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User
 
   field :name, type: String
   field :surname, type: String
+  field :date_of_birth, type: Date
+  field :place_of_birth, type: String
+  field :street, type: String
+  field :city, type: String
+  field :postal_code, type: String
+  field :PESEL, type: String
+  field :id_number, type: String
+  field :id_serial, type: String
+  field :phone, type: String
+  field :NIP, type: String
+  field :email, type: String
 
 end
 
