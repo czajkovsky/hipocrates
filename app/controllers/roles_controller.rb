3bccf9b0fc5fe17102c8e54ee9f3bc6b0f041183
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-09 17:35:12 +0100

    CRUD for role

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,33 @@
+class RolesController < ApplicationController
+
+  expose(:roles)
+  expose(:role, attributes: :permitted_params)
+
+  def create
+    if role.save
+      redirect_to roles_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if role.save
+      redirect_to roles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    role.destroy
+    redirect_to roles_path
+  end
+
+  private
+
+  def permitted_params
+    params.require(:role).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Hipocrates::Application.routes.draw do
 
   root to: 'dashboard#index'
   resources :users
+  resources :roles, except: :show
 
 end
