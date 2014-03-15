4d1bd6c4f42197a53aadd6573eafd6199bf3a3e7
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-15 14:43:45 +0100

    handle session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :current_user
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
