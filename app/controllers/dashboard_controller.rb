4ae6ec050c1dcc2e43e20625f63755b71f403021
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-22 22:43:01 +0100

    doctors dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
   expose(:patient_visits){ Visit.where(patient: current_user).decorate }
   expose(:pending_visits){ Visit.pending.decorate }
   expose(:confirmed_visits){ Visit.confirmed.decorate }
+  expose(:doctor_visits){ Visit.where(doctor: current_user).decorate }
 
 end
