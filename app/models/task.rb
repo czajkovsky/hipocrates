e0f29ac9e958397c4c3501271a36746732de0695
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-29 14:17:46 +0100

    fetch recognitions

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,9 +5,19 @@ class Task
     ex = Roo::Excel.new("public/meds.xls")
     ex.default_sheet = ex.sheets[0]
     4.upto(10823) do |line|
-      name = ex.cell(line,'B')
-      form = ex.cell(line,'D')
+      name = ex.cell(line, 'B')
+      form = ex.cell(line, 'D')
       Med.create(name: name, form: form) if name.present? and form.present?
+    end
+  end
+
+  def self.fetch_recognitions_from_excel
+    ex = Roo::Excel.new('public/recognitions.xls')
+    ex.default_sheet = ex.sheets[1]
+    2.upto(34342) do |line|
+      icd10 = ex.cell(line, 'A')
+      name = ex.cell(line, 'B')
+      Recognition.create(icd10: icd10, name: name) if name.present? and icd10.present?
     end
   end
 
